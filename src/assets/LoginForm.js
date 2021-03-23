bd217ff237b4c9d4f117764286a6c60718558e42
Kameliqp95 <kameliqp95@gmail.com>
2021-03-23 20:13:02 +0200

    formik validation

--- src/assets/LoginForm.js
+++ src/assets/LoginForm.js
@@ -1,16 +1,38 @@
 import React from 'react'
-
+import { useFormik } from 'formik'
+const initialValues= {
+    username: 'Username1',
+    password: 'Password1'
+}
+const onSubmit= values => {console.log('formData', values)}
+const validate= values => {
+    let errors = {}
+    if(!values.username) {
+        errors.username='requiered username'
+    }
+    if(!values.password) {
+        errors.password='requiered password'
+    }
+    return errors
+            } 
 function LoginForm() {
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+     })
+        console.log('formValues', formik.values)
+        console.log('formErrors', formik.errors)
     return (
-        <div>
-            <form>
+        <div class="form-style-3">
+            <form onSubmit={formik.handleSubmit}>
                 <label  htmlFor='username'>Username</label>
-                <input type="text" id='username' name='username' />
+                <input type="text" id='username' name='username' onChange={formik.handleChange} value={formik.values.username} />
                 <br />
                 <label  htmlFor='password'>Password</label>
-                <input type="password" id='password' name='password' />
+                <input type="password" id='password' name='password' onChange={formik.handleChange} value={formik.values.password} />
                 <br />
-                <button>Submit</button>
+                <button type="submit">Submit</button>
 
             </form>
         </div>
--- src/assets/RegistrationForm.js
+++ src/assets/RegistrationForm.js
@@ -1,22 +1,67 @@
 import React from 'react'
-
+import { useFormik } from 'formik'
+const initialValues= {
+    username: 'Username1',
+    password: 'Password1',
+    firstName: 'firstName1',
+    lastName: 'lastName1',
+    city: 'City1',
+    address: 'Address1'
+}
+const onSubmit= values => {console.log('formData', values)}
+const validate= values => {
+    let errors = {}
+    if(!values.username) {
+        errors.username='requiered username'
+    }
+    if(!values.password) {
+        errors.password='requiered password'
+    }
+    if(!values.firstName) {
+        errors.firstName='requiered firstname'
+    }
+    if(!values.lastName) {
+        errors.lastName='requiered lastname'
+    }
+    if(!values.city) {
+        errors.city='requiered city'
+    }
+    if(!values.address) {
+        errors.address='requiered address'
+    }
+    return errors
+            } 
 function RegistrationForm() {
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+     })
+        console.log('formValues', formik.values)
+        console.log('formErrors', formik.errors)
     return (
-        <div>
-            <form>
+        <div class="form-style-3">
+             <form onSubmit={formik.handleSubmit}>
                 <label  htmlFor='username'>Username</label>
-                <input type="text" id='username' name='username' />
+                <input type="text" id='username' name='username' onChange={formik.handleChange} value={formik.values.username} />
                 <br />
                 <label  htmlFor='password'>Password</label>
-                <input type="password" id='password' name='password' />
+                <input type="password" id='password' name='password' onChange={formik.handleChange} value={formik.values.password} />
                 <br />
-                <label  htmlFor='email'>Email</label>
-                <input type="email" id='email' name='email' /> 
+                <label  htmlFor='firstName'>firstName</label>
+                <input type="text" id='firstName' name='firstName' onChange={formik.handleChange} value={formik.values.firstName} />
                 <br />
-                <label  htmlFor='dateOfBirth'>Email</label>
-                <input type="date" id='dateOfBirth' name='dateOfBirth' />   
-                <br />                            
-                <button>Submit</button>
+                <label  htmlFor='lastName'>lastName</label>
+                <input type="text" id='lastName' name='lastName' onChange={formik.handleChange} value={formik.values.lastName} />
+                <br />
+                <label  htmlFor='city'>city</label>
+                <input type="text" id='city' name='city' onChange={formik.handleChange} value={formik.values.city} />
+                <br />
+                <label  htmlFor='address'>address</label>
+                <input type="text" id='address' name='address' onChange={formik.handleChange} value={formik.values.address} />
+                <br />
+                <button type="submit">Submit</button>
+
             </form>
         </div>
     )
